9d5f771bd7b582b7b51e7d15211e79320638cc6a
joaogsmoreira <1150672@isep.ipp.pt>
2023-02-05 14:35:06 +0000

    Initial commit

---
+++ env_activate.sh
@@ -0,0 +1,8 @@
+# CUDA - Consider adding this to .bashrc
+export PATH=/usr/local/cuda-11.1/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.1/bin:$LD_LIBRARY_PATH
+
+# NUMBA - Some older numba versions require this
+export NUMBAPRO_CUDA_DRIVER=/usr/lib/x86_64-linux-gnu/libcuda.so
+export NUMBAPRO_NVVM=/usr/local/cuda-11.1/nvvm/lib64/libnvvm.so
+export NUMBAPRO_LIBDEVICE=/usr/local/cuda-11.1/nvvm/libdevice
