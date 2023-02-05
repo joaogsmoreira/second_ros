895cdebbd9d88682f6029a95c66558d79a1ae236
joaogsmoreira <1150672@isep.ipp.pt>
2023-02-05 15:41:47 +0000

    Fixed typo

--- env_activate.sh
+++ env_activate.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/local/cuda-11.1/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11.1/bin:$LD_LIBRARY_PATH
 
-# NUMBA - Some older numba versions require this
+# NUMBA - Some older versions require this
 export NUMBAPRO_CUDA_DRIVER=/usr/lib/x86_64-linux-gnu/libcuda.so
 export NUMBAPRO_NVVM=/usr/local/cuda-11.1/nvvm/lib64/libnvvm.so
 export NUMBAPRO_LIBDEVICE=/usr/local/cuda-11.1/nvvm/libdevice
