dd53561aa73af05585ec166de9a23092a4218c4a
joaogsmoreira <1150672@isep.ipp.pt>
2023-02-26 19:32:49 +0000

    Removing deprecated numba defines

--- env_activate.sh
+++ env_activate.sh
@@ -3,6 +3,6 @@ export PATH=/usr/local/cuda-11.1/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11.1/bin:$LD_LIBRARY_PATH
 
 # NUMBA - Some older versions require this
-export NUMBAPRO_CUDA_DRIVER=/usr/lib/x86_64-linux-gnu/libcuda.so
-export NUMBAPRO_NVVM=/usr/local/cuda-11.1/nvvm/lib64/libnvvm.so
-export NUMBAPRO_LIBDEVICE=/usr/local/cuda-11.1/nvvm/libdevice
+#export NUMBAPRO_CUDA_DRIVER=/usr/lib/x86_64-linux-gnu/libcuda.so
+#export NUMBAPRO_NVVM=/usr/local/cuda-11.1/nvvm/lib64/libnvvm.so
+#export NUMBAPRO_LIBDEVICE=/usr/local/cuda-11.1/nvvm/libdevice
